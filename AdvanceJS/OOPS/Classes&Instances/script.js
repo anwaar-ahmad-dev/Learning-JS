6c4281be0e6b84049640d0aa44d8dac4f4f2fe00
anwaar.ahmad.dev@gmail.com <anwaar.ahmad.dev@gmail.com>
2026-02-27 23:44:46 +0530

    Classes and Instances

--- AdvanceJS/OOPS/Classes&Instances/script.js
+++ AdvanceJS/OOPS/Classes&Instances/script.js
@@ -1,3 +1,5 @@
+// classes and instances.
+
 class CreatePencil {
     constructor(name, company, price, color) {
         this.name = name;
@@ -5,6 +7,18 @@ class CreatePencil {
         this.price = price;
         this.color = color;
     }
+
+    // when erase is called by p1 the elements with black color are removed from the page.
+    // same goes with p2 , the blue color elements are removed..
+    erase() {
+        document.body.querySelectorAll("h1").forEach(elem => {
+            if (elem.style.color === this.color) {
+                elem.remove();
+            }
+        });
+    }
+
+    // used to write and create the h1 with custom text each time 
 
     write(text) {
         let h1 = document.createElement("h1");
